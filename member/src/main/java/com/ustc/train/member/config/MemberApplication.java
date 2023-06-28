12e9abfccf20aaf43a67c2a27d3e1fa731bb0eb5
longxu0509 <longxu@mail.ustc.edu.cn>
2023-06-28 20:51:54 +0800

    feat: 增加LogAspect 拦截所有的请求，打印日志

--- member/src/main/java/com/ustc/train/member/config/MemberApplication.java
+++ member/src/main/java/com/ustc/train/member/config/MemberApplication.java
@@ -1,15 +1,11 @@
 package com.ustc.train.member.config;
 
-import org.apache.juli.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 /**
  * @author xulong
@@ -25,6 +21,6 @@ public class MemberApplication {
         SpringApplication app = new SpringApplication(MemberApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！");
-        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
     }
 }
