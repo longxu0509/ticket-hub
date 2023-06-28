9511955c97d5ef8252d33ff054a370235d7f8b4b
longxu0509 <longxu@mail.ustc.edu.cn>
2023-06-28 19:46:26 +0800

    feat: 修改启动日志，增加个性化banner, 修改logback日志

--- member/src/main/java/com/ustc/train/member/config/MemberApplication.java
+++ member/src/main/java/com/ustc/train/member/config/MemberApplication.java
@@ -1,8 +1,14 @@
 package com.ustc.train.member.config;
 
+import org.apache.juli.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,9 +19,12 @@ import org.springframework.stereotype.Component;
 @SpringBootApplication
 @ComponentScan("com.ustc")
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
-
 }
